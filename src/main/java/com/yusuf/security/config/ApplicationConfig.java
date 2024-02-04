6bfc7e7fffb78497263b171003963e8fd814ee5b
YusufDagdeviren <ddyusufdd@gmail.com>
2024-02-05 01:52:59 +0300

    ApplicationConfig completed

--- src/main/java/com/yusuf/security/config/ApplicationConfig.java
+++ src/main/java/com/yusuf/security/config/ApplicationConfig.java
@@ -16,4 +16,19 @@ public class ApplicationConfig {
         return username -> repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+    @Bean
+    private PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
